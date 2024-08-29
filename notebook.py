2cee61f71e19cc34bb42ce0293e0e9e0c6b1d8e6
AshDavid12 <gaashdavid@gmail.com>
2024-08-29 11:32:35 +0300

    attempt for docker image

--- notebook.py
+++ notebook.py
@@ -8,7 +8,7 @@ load_dotenv('.env')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 RUN_POD_API_KEY = os.getenv('RUN_POD_API_KEY')
 RUNPOD_ENDPOINT_ID =os.getenv('RUNPOD_ENDPOINT_ID')
-openai.api_key = OPENAI_API_KEY
+
 
 mp3_data = open('me-hebrew.wav', 'rb').read()
 data = base64.b64encode(mp3_data).decode('utf-8')
